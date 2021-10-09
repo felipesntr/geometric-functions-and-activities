db6a1c1a369a0ec873c1e170240d2566492fccec
Felipe Santos <rfelipe273@gmail.com>
2021-10-09 10:02:00 -0300

    add 'centroid' function

---
+++ src/Centroid.hs
@@ -0,0 +1,14 @@
+module Centroid where
+import Area
+
+
+-- | The 'centroid' function return the centroid point of a polygon.
+centroid :: Polygon -> Point
+centroid polygon = (c_x, c_y)
+    where 
+        c_x = sum [ ((x_1 + x_2) * (x_1*y_2 - x_2* y_1)) | ((x_1, y_1), (x_2, y_2)) <- pairList ]
+                / (6 * areaPolygon polygon)
+        c_y = sum [ ((y_1 + y_2) * (x_1*y_2 - x_2*y_1)) | ((x_1, y_1), (x_2, y_2)) <- pairList ]
+                / (6 * areaPolygon polygon)
+        polygon' = polygon ++ (take 1 polygon)
+        pairList = zip (polygon') (tail polygon')

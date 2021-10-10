5ba7ea34c4b26a955d7d526b15194d41946e5b50
Felipe Santos <rfelipe273@gmail.com>
2021-10-10 00:06:40 -0300

    change: removed redudant brackets

--- src/Geometric/Centroid.hs
+++ src/Geometric/Centroid.hs
@@ -1,13 +1,16 @@
 module Geometric.Centroid where
-import Geometric.Area
+import Geometric.Area ( Polygon, Point, areaPolygon )
 
 -- | The 'centroid' function return the centroid point of a polygon.
 centroid :: Polygon -> Point
 centroid polygon = (c_x, c_y)
-    where 
-        c_x = sum [ ((x_1 + x_2) * (x_1*y_2 - x_2* y_1)) | ((x_1, y_1), (x_2, y_2)) <- pairList ]
+    where
+        c_x =  sum [  (x_1 + x_2) * (x_1*y_2 - x_2* y_1) | ((x_1, y_1), (x_2, y_2)) <- pairList ]
                 / (6 * areaPolygon polygon)
-        c_y = sum [ ((y_1 + y_2) * (x_1*y_2 - x_2*y_1)) | ((x_1, y_1), (x_2, y_2)) <- pairList ]
+        c_y =  sum [  (y_1 + y_2) * (x_1*y_2 - x_2*y_1) | ((x_1, y_1), (x_2, y_2)) <- pairList ]
                 / (6 * areaPolygon polygon)
-        polygon' = polygon ++ (take 1 polygon)
-        pairList = zip (polygon') (tail polygon')
+        polygon' = polygon ++ take 1 polygon
+        pairList = zip polygon' (tail polygon')
+
+
+        

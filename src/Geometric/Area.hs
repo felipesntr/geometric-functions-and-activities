09a8662ffb53a9f24b7877eb151b7867561012e4
Felipe Santos <rfelipe273@gmail.com>
2021-10-10 00:11:26 -0300

    change: removed redundant brackets

--- src/Geometric/Area.hs
+++ src/Geometric/Area.hs
@@ -6,10 +6,10 @@ type Polygon = [Point]
 
 -- | The 'areaPolygon' function return the area of a polygon.
 areaPolygon :: Polygon -> Float
-areaPolygon polygon = 
-    abs (0.5 * sum [ 
+areaPolygon polygon =
+    abs (0.5 * sum [
         (x_1*y_2) - (x_2*y_1) | ((x_1, y_1), (x_2, y_2)) <- pairList])
     where
         polygon' = polygon ++ take 1 polygon
-        pairList = zip (polygon') (tail polygon')
+        pairList = zip polygon' (tail polygon')
 
